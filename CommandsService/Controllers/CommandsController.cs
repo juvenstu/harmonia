e11a9f3962d1ab5d0f4465dae3c9f3371df07ced
Juvens Tuyizere <144620325+juvenstu@users.noreply.github.com>
2024-07-14 18:25:52 +0200

    commandsservice: create get command for platform action result

--- CommandsService/Controllers/CommandsController.cs
+++ CommandsService/Controllers/CommandsController.cs
@@ -15,9 +15,21 @@ public class CommandsController(ICommandRepo repository, IMapper mapper) : Contr
     [HttpGet]
     public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
     {
-        Console.WriteLine($"info: Get commands for platform with id: {platformId}");
+        Console.WriteLine($"info: Get commands for platform by ID: {platformId}");
         if (!_repository.PlatformExists(platformId)) return NotFound();
         var commands = _repository.GetCommandsForPlatform(platformId);
+
         return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+    }
+
+    [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+    public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+    {
+        Console.WriteLine($"info: Get command for platform by both IDs: {platformId} / {commandId}");
+        if (!_repository.PlatformExists(platformId)) return NotFound();
+        var command = _repository.GetCommand(platformId, commandId);
+        if (command == null) return NotFound();
+
+        return Ok(_mapper.Map<CommandReadDto>(command));
     }
 }
