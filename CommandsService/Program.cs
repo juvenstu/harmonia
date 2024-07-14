75cc917516169929026ce352e24308b19205bc89
Juvens Tuyizere <144620325+juvenstu@users.noreply.github.com>
2024-07-14 17:21:15 +0200

    commandsservice: add db context

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,9 +1,14 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("CommandsService"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 
 var app = builder.Build();
 
