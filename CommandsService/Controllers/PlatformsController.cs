e65ab4822588e4841c1ed78f0b6e7628887634d3
Juvens Tuyizere <144620325+juvenstu@users.noreply.github.com>
2024-07-14 17:43:00 +0200

    commandsservice: add get platforms action result

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,21 +1,30 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers;
 
 [ApiController]
 [Route("api/c/[controller]")]
-public class PlatformsController : ControllerBase
+public class PlatformsController(ICommandRepo repository, IMapper mapper) : ControllerBase
 {
-    public PlatformsController()
+    private readonly ICommandRepo _repository = repository;
+    private readonly IMapper _mapper = mapper;
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
     {
-        // something here
+        Console.WriteLine("info: Getting platforms from CommandsService");
+        var platforms = _repository.GetAllPlatforms();
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
     }
 
     [HttpPost]
     [ProducesResponseType<string>(StatusCodes.Status200OK)]
     public ActionResult TestInBoundConnection()
     {
-        Console.WriteLine("--> Inbound POST # Command Service");
+        Console.WriteLine("info: Inbound POST # Command Service");
         return Ok("Inbound test from platforms controller is ok!");
     }
 }
