0a94453a25ecf2c76678e2a3e5ccf5fd6a2c707f
Juvens Tuyizere <144620325+juvenstu@users.noreply.github.com>
2024-07-14 16:37:06 +0200

    commandsservice: create data transfer objects

---
+++ CommandsService/Dtos/CommandReadDto.cs
@@ -0,0 +1,9 @@
+namespace CommandsService.Dtos;
+
+public class CommandReadDto
+{
+    public int Id { get; set; }
+    public required string HowTo { get; set; }
+    public required string CommandLine { get; set; }
+    public int PlatformId { get; set; }
+}
