5e1a55b030c490243ce063eb0dbd7f30355bb814
Juvens Tuyizere <144620325+juvenstu@users.noreply.github.com>
2024-07-18 15:47:07 +0200

    CommandsService: Add DB preparation to seed data using gRPC

---
+++ CommandsService/Data/PrepDb.cs
@@ -0,0 +1,27 @@
+using CommandsService.Models;
+using CommandsService.SyncDataServices.Grpc;
+
+namespace CommandsService.Data;
+
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder applicationBuilder)
+    {
+        using var serviceScope = applicationBuilder.ApplicationServices.CreateScope();
+        var grpcClient = serviceScope.ServiceProvider.GetRequiredService<IPlatformDataClient>();
+        var platforms = grpcClient.ReturnAllPlatforms();
+
+        SeedData(serviceScope.ServiceProvider.GetRequiredService<ICommandRepo>(), platforms);
+    }
+
+    private static void SeedData(ICommandRepo repo, IEnumerable<Platform> platforms)
+    {
+        Console.WriteLine("info: Seeding new platforms...");
+        foreach (var platform in platforms)
+        {
+            if (!repo.ExternalPlatformExists(platform.ExternalId))
+                repo.CreatePlatform(platform);
+            repo.SaveChanges();
+        }
+    }
+}
